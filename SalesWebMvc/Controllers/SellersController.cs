23864051e530748b810538af77ddc360a514bace
jo.f.a.snathan@gmail.com <jo.f.a.snathan@gmail.com>
2024-10-22 13:04:41 -0300

    Seller details and eager loading

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -28,26 +28,27 @@ namespace SalesWebMvc.Controllers
             var departments = _departmentService.FindAll();
             var viewModel = new SellerFormViewModel { Departments = departments };
             return View(viewModel);
-     
+
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create(Seller seller) 
-        { 
-          _sellerService.Insert(seller);
-          return RedirectToAction(nameof(Index));
-        
-        
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
+
+
         }
 
         public IActionResult Delete(int? id)
         {
             if (id == null)
-            { 
-            
-            return NotFound();
-            
+            {
+
+                return NotFound();
+
             }
+
 
             var obj = _sellerService.FindById(id.Value);
             if (obj == null)
@@ -67,5 +68,26 @@ namespace SalesWebMvc.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+
+                return NotFound();
+
+            }
+
+
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+
+            }
+
+            return View(obj);
+
+
+        }
     }
 }
