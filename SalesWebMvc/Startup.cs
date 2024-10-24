3a36658b88528ad9f71cc79800d13084fc9069d6
jo.f.a.snathan@gmail.com <jo.f.a.snathan@gmail.com>
2024-10-24 15:37:55 -0300

    Implementing simple search

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -13,6 +13,23 @@ using SalesWebMvc.Models;
 using SalesWebMvc.Services;
 using Microsoft.AspNetCore.Localization;
 using System.Globalization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Models;
+using SalesWebMvc.Data;
+using SalesWebMvc.Services;
+
+
 namespace SalesWebMvc
 {
     public class Startup
@@ -36,6 +53,7 @@ namespace SalesWebMvc
             services.AddScoped<SeedingService>();
             services.AddScoped<SellerService>();
 			services.AddScoped<DepartmentService>();
+            services.AddScoped<SalesRecordService>();
 		}
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
